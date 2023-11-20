874ab6d245b56a217abaf3d09d27a87e55d5a8e0
Joseph <joseph.dena@student.moringaschool.com>
2023-11-20 07:59:34 +0300

    First commit

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,23 @@
+// import libraries needed for testing
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
+// import the component you wrote
 import Article from "../components/Article";
 
+// test the component
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+
+  // add this line
+  screen.debug();
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
+
+
+
+
+
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,11 @@
+
+// Article.js
+
 import React from "react";
 
 function Article() {
-  return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+  return <div>please pass this test</div>;
 }
 
 export default Article;
+
